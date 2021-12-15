612912d5a934ba8de7e809ac2bc2c04a42dfa1a2
amirhosseinbagheri <ahbagheri01@gmail.com>
2021-12-16 00:47:28 +0330

    added 3

---
+++ P3/Q2.cpp
@@ -0,0 +1,84 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+typedef vector<vector<int>> Adj_matrix;
+void print_adj(Adj_matrix& adj){
+    cout<<endl;
+    for(int i = 0 ; i < adj.size() ; i++){
+        for (auto v : adj[i]){
+            cout<<v<<" ";
+        }
+        cout<<endl;
+    }
+}
+int dfs(Adj_matrix& adj,int* visited,int s,int p,int t,int l){
+    if (s == t){
+        return l+1;
+    }
+    print_adj(adj);
+    visited[s] = true;
+    int newL = 0;
+    for (auto v : adj[s]){
+        if (v == p || visited[v]){
+            continue;
+        }
+        newL = max(newL,dfs(adj,visited,v,s,t,l+1));
+    }
+    visited[s] = false;
+    return newL;
+}
+int longest_path(Adj_matrix& adj,int* visited,int s,int t){
+    if (s == t){
+        return 0;
+    }
+    print_adj(adj);
+    int l = 0;
+    visited[s] = true;
+    int newL = l;
+    for (auto v : adj[s]){
+        newL = max(newL,dfs(adj,visited,v,s,t,l));
+    }
+    visited[s] = false;
+    return newL;
+}
+void print_table(int** res , int n){
+    cout<<endl;
+    int i , j ;
+    for( i = 0 ; i < n ; i++){
+        for( j = 0 ; j < n-1 ; j++){
+            cout<<res[i][j]<<" ";
+        }
+        cout<<res[i][j]<<endl;
+    }
+}
+int main(){
+    int n,m,u,v;
+    cin >> n >> m;
+    int** res = (int**) malloc( n*sizeof(int*));
+    for(int i = 0 ; i < n ; i++){
+        res[i] = (int*) malloc( n*sizeof(int));
+    }
+    Adj_matrix adj(n);
+    for(int i = 0 ; i < m ; i++){
+        cin>>u>>v;
+        adj[v-1].push_back(u-1);
+        adj[u-1].push_back(v-1);
+    }
+    print_adj(adj);
+
+    int* visited = (int*) calloc(n,sizeof(bool));
+    for(int i = 0 ; i < n ; i++){
+        res[i][i] = 0;
+        for(int j = i+1 ; j < n ; j++){
+            print_table(res,n);
+            res[i][j] = res[j][i] = longest_path(adj,visited,i,j);
+        }
+    }
+    print_table(res,n);
+    free(visited);
+    for(int i = 0 ; i < n ; i++){
+        free(res[i]);
+    }
+    free(res);
+}
+
